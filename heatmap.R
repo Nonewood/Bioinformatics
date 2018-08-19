07374535173cf2c8e3942b2b346dfeb6ddd55bdc
Nonewood <31466969+Nonewood@users.noreply.github.com>
2018-08-19 14:26:43 +0800

    Update heatmap.R

--- heatmap.R
+++ heatmap.R
@@ -3,5 +3,5 @@
 library('pheatmap')
 dt = read.table('braycurtis.txt', header=T)
 expose = read.table('expose_diversity', header=T, check.names=FALSE)
-expose_group = read.table('Sample_information_detail.xls', header=T, row.names=1, comment.char='', check.names=F) #分组信息，行名需是距离矩阵的列名  
+expose_group = read.table('Sample_information_detail.txt', header=T, row.names=1, comment.char='', check.names=F) #分组信息，行名需是距离矩阵的列名  
 pheatmap(expose, fontsize=9,annotation_col=expose_group, filename='expose_bray.pdf')
