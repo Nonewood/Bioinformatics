80f7c2a46eeb5615dc1c54ba1d884c5ed0f96dfb
Nonewood <31466969+Nonewood@users.noreply.github.com>
2018-08-23 09:25:23 +0800

    Update upset.R

--- upset.R
+++ upset.R
@@ -1,4 +1,6 @@
-library(UpSetR)
-gene <- read.csv('Data/upset.csv', header = T, sep = '\t')
+library(UpSetR，lib.loc = "/installpath/R_lib") #指定加载包的路径
+pdf("upset.pdf", width=8,height=6, onefile = FALSE) #输出到 pdf 文件，并且取消第一页的空白页
+gene <- read.csv('Data/upset.csv', header = T, sep = '\t') 
 upset(gene, order.by = "degree", mb.ratio = c(0.70, 0.30))
+dev.off()
 # 就这么简单，成图..
