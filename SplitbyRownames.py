e56e40d373ee447ac1ef1c6be153bb18a781d705
Nonewood <31466969+Nonewood@users.noreply.github.com>
2018-09-24 15:00:34 +0800

    Update SplitbyRownames.py

--- SplitbyRownames.py
+++ SplitbyRownames.py
@@ -4,3 +4,7 @@ expose_diversity = dt.loc[dt.index.str.contains('-a') == False, dt.columns.str.c
 recovery_diversity = dt.loc[dt.index.str.contains('-a'), dt.columns.str.contains('-a')] # 行名包含 '-a' 的行；
 expose_diversity.to_csv('expose_diversity', sep='\t')
 recovery_diversity.to_csv('recovery_diversity', sep='\t')
+
+#补充用法，增加单独筛选行或者列的用法，一旦不用就老是忘记....
+expose_dt = dt.loc[:,dt.columns.str.contains('-a') == False]
+recovery_dt = dt.loc[:,dt.columns.str.contains('-a')]  #注意冒号的使用，代表全部！
