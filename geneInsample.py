e780f66082abc5f2bc460c3a26c5c93531b5bc4f
Nonewood <31466969+Nonewood@users.noreply.github.com>
2018-09-24 10:08:41 +0800

    Update geneInsample.py
    更新这个脚本，优化了脚本，解决掉一个 bug，增加了图形的输出.
    之前写得脚本太蠢了，怎么会有这么蠢的自己...

--- geneInsample.py
+++ geneInsample.py
@@ -31,47 +31,48 @@ else:
 df.columns.values[0] = 'geneID'
 df = df.set_index('geneID')
 totalsample = len(df.columns)
-sampleNum = (df!=0).sum(axis=1)
-totalGene = len(sampleNum)
+sampleNum = (df!=0).sum(axis=1)  # the sample numers of each gene, including genes exist in no samples
+geneOf_0 = sum(sampleNum == 0)  # judge the genes number  exist in no samples
+# error
+if geneOf_0:
+	print("some genes do not exist in all samples! please check your file!\nThe corresponding gene numbers is " + str(geneOf_0) + ' :)')
+	#exit()
+else:
+	next
 
-gene_10 = sampleNum[sampleNum >= totalsample*0.1]
-per_10 = len(gene_10)
+totalGene = sum(sampleNum != 0)
+
+out = open (outpath,'w')
+out.write('\t'.join(['threshold','sampleNumber','geneNumber','rate']) + '\n')
+out.write('\t'.join(['>0', '1', str(totalGene), '1']) + '\n')
+for x in range(10,91,10):
+	threshold = '>=' + str(x) + '%'
+	sampleNumber = int(totalsample*(x/100)) + 1
+	geneNumber = len(sampleNum[sampleNum >= sampleNumber])
+	geneRate =  round(geneNumber/totalGene,2)
+	out.write('\t'.join([threshold,str(sampleNumber),str(geneNumber),str(geneRate)]) + '\n')
+out.close()
+
+### plot ###
+import seaborn as sns
+import matplotlib.pyplot as plt
+dt = pd.read_table(outdir + '/geneInsample.txt', sep='\t', header=0)
+sns.set_style("ticks")
+p = sns.barplot(x=dt["sampleNumber"],y=dt["geneNumber"])
+p.axes.set_title("geneINsample", size=14)
+p.set_xlabel("sample numbers(>=)", size=12)
+p.set_ylabel("gene numbers", size=12)
+p.tick_params(labelsize=10)
+fig = p.get_figure()
+fig.savefig(outdir+ "geneINsample.pdf")
+
 #print out the genes exist more than in 10% samples
+'''
+sampleOfTen = int(totalsample*0.1)
+geneOfTen = sampleNum[sampleNum >= sampleOfTen]
 with open('GeneOfTen.txt','w') as gene:
-    gene_10.to_csv(gene, sep='\t')
-per_10_rate = round(per_10/totalGene,2)
-
-#增加代码使其能够输出 10% 样品中存在基因的丰度表,这么简单让自己想半天...
-geneprofile_10 = df[sampleNum >= totalsample*0.1]
+	geneOfTen.to_csv(gene, sep='\t')
+geneprofile_10 = df[sampleNum >= sampleOfTen]
 with open('geneprofile_10','w') as profile:
     geneprofile_10.to_csv(profile, sep='\t')
-
-per_20 = len(sampleNum[sampleNum >= totalsample*0.2])
-per_20_rate = round(per_20/totalGene,2)
-per_30 = len(sampleNum[sampleNum >= totalsample*0.3])
-per_30_rate = round(per_30/totalGene,2)
-per_40 = len(sampleNum[sampleNum >= totalsample*0.4])
-per_40_rate = round(per_40/totalGene,2)
-per_50 = len(sampleNum[sampleNum >= totalsample*0.5])
-per_50_rate = round(per_50/totalGene,2)
-per_60 = len(sampleNum[sampleNum >= totalsample*0.6])
-per_60_rate = round(per_60/totalGene,2)
-per_70 = len(sampleNum[sampleNum >= totalsample*0.7])
-per_70_rate = round(per_70/totalGene,2)
-per_80 = len(sampleNum[sampleNum >= totalsample*0.8])
-per_80_rate = round(per_80/totalGene,2)
-per_90 = len(sampleNum[sampleNum >= totalsample*0.9])
-per_90_rate = round(per_90/totalGene,2)
-with open (outpath,'w') as out:
-    out.write('\t'.join(['threshold','number','rate']) + '\n')
-    out.write('\t'.join(['>=0', str(totalGene), '1']) + '\n')
-    out.write('\t'.join(['>=10%', str(per_10), str(per_10_rate)]) + '\n')
-    out.write('\t'.join(['>=20%', str(per_20), str(per_20_rate)]) + '\n')
-    out.write('\t'.join(['>=30%', str(per_30), str(per_30_rate)]) + '\n')
-    out.write('\t'.join(['>=40%', str(per_40), str(per_40_rate)]) + '\n')
-    out.write('\t'.join(['>=50%', str(per_50), str(per_50_rate)]) + '\n')
-    out.write('\t'.join(['>=60%', str(per_60), str(per_60_rate)]) + '\n')
-    out.write('\t'.join(['>=70%', str(per_70), str(per_70_rate)]) + '\n')
-    out.write('\t'.join(['>=80%', str(per_80), str(per_80_rate)]) + '\n')
-    out.write('\t'.join(['>=90%', str(per_90), str(per_90_rate)]) + '\n')
- 
+'''
