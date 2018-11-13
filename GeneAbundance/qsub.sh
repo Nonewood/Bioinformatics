9b1b62322e143d21e6114a871cd33a4d9b13d208
Nonewood <31466969+Nonewood@users.noreply.github.com>
2018-11-13 10:35:17 +0800

    Update qsub.sh

--- GeneAbundance/qsub.sh
+++ GeneAbundance/qsub.sh
@@ -1,11 +1,11 @@
-for file in /ifswh1/BC_COM_P2/F18FTSNCWLJ0169/HUMnzsM/HUMrfvM/metawrap/GeneAbundance/Outdir/Shell/*
+for file in shell_dir/*
 do 
 	if test -d $file
 	then
 		cd $file 
 		for script in ${file}/*sh
 		do
-			qsub -cwd -l vf=5G,p=1 -P HUMrfvM -q bc.q $script
+			qsub -cwd -l vf=XG,p=1 -P XXX -q xx.q $script
 		done
 	fi
 done
