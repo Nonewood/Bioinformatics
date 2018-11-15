9899d64edbe6cb184f4b98aafd1fdcf59d256586
Nonewood <31466969+Nonewood@users.noreply.github.com>
2018-11-15 14:40:35 +0800

    Update scp.sh

--- rat/scp.sh
+++ rat/scp.sh
@@ -1,4 +1,4 @@
 for x in `ls *pdf`
 do
-	expect /ifshk7/BC_PS/wangpeng7/Script/scp.expect $x /Users/tongxueer/Documents/20180116-大鼠项目/genus
+	expect cp.expect $x /Users/tongxueer/Documents/20180116-大鼠项目/genus
 done
