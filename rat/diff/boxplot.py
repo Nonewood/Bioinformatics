20bef3d87c6ee6715aaa432a468766458a444058
Nonewood <31466969+Nonewood@users.noreply.github.com>
2018-12-14 16:20:57 +0800

    Add files via upload

--- rat/diff/boxplot.py
+++ rat/diff/boxplot.py
@@ -17,10 +17,11 @@ parser.add_argument('-c','--color', help = "the color scheme,eg 487eb3:d2382c.")
 parser.add_argument('-d','--diff', help = "the filtered different test result file, eg filter_EL_Cig.E-liquid-Cigarette.wilcox.test.xls.")
 parser.add_argument('-n','--number', help = "the number of tax for boxplot,eg 20. default is 20.", type=int, nargs='?')
 parser.add_argument('-f','--filter_type', help = "the filter type of different result,eg pvalue. default is qavlue", nargs='?')
+parser.add_argument('-p','--prefix', help = "the prefix for output,eg genus.")
 parser.add_argument('-o','--outdir', help = "the output directory")
 args=parser.parse_args()
-(abd_file,group,color,diff_result,outdir) = (args.abd,args.group,args.color,args.diff,args.outdir)
-par = [abd_file,group,color,diff_result,outdir]
+(abd_file,group,color,diff_result,prefix,outdir) = (args.abd,args.group,args.color,args.diff,args.prefix,args.outdir)
+par = [abd_file,group,color,diff_result,prefix,outdir]
 if not all(par):
 	parser.print_help()
 	exit()
@@ -49,7 +50,7 @@ mean_order = pd.read_table('tax_mean_order.txt', header=0, index_col=0)
 plot_tax = numpy.unique(mean_order.sort_values(by = 'Mean', ascending=False)[:tax_number*2].index)[:tax_number]
 
 with open(abd_file, 'r') as abd, open('boxplot.txt', 'w') as out:
-    print('ID\tAbd\tGroup\n', file=out)
+    print('ID\tAbd\tGroup', file=out)
     head = abd.readline().strip('\n').split('\t')
     for line in abd:
         line = line.strip('\n').split('\t')
@@ -60,7 +61,7 @@ with open(abd_file, 'r') as abd, open('boxplot.txt', 'w') as out:
                 else:
                     print(line[0] + '\t'+ line[index] + '\t' + group_list[1], file=out)
 
-rscript = 'Rscript boxplot.R boxplot.txt ' + group + ' ' + color + ' ' + diff_result + ' ' + filter_type
+rscript = 'Rscript /ifshk7/BC_PS/wangpeng7/Script/diff/boxplot.R boxplot.txt ' + group + ' ' + color + ' ' + diff_result + ' ' + filter_type + ' ' + prefix
 print(rscript)
 os.system(rscript)
 # 删除过程文件
