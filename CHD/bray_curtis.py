d9a16d039bb93d324f097d7c90ba63dcf41db8c3
Nonewood <31466969+Nonewood@users.noreply.github.com>
2019-09-24 17:51:10 +0800

    Create bray_curtis.py
    早期写的了，感觉现在写会有更加简单的方法... 输入文件没有放在 github 上边；

---
+++ CHD/bray_curtis.py
@@ -0,0 +1,32 @@
+##属水平 beta 多样性处理, 只比较三组组内的距离
+import pandas as pd
+dt = pd.read_table('braycurtis.txt', index_col = 0)  #距离矩阵文件
+group = pd.read_table('info.xls', index_col=0) #表型文件
+group_dict = group['Group'].to_dict() #生成样品-组名的字典
+
+##两两样品名作为键，距离值作为值，生成字典..
+distance = dict()
+for Id1 in dt.columns:
+    for Id2 in dt.columns:
+        key = Id1 + ':' + Id2
+        key_temp = Id2 + ':' + Id1
+        value = dt.loc[Id1,Id2]
+        if (key in distance) or (key_temp in distance): # 判断是否有重复的
+            continue
+        else:
+            distance[key] = value
+            
+out = open('intra_distance.txt','w')  # 生成画 boxplot 的文件
+print('individuals\tdistance\tgroup', file=out)
+n = 0
+for key in distance:
+    key_split = key.split(':')
+    if key_split[0] == key_split[1]: #如果是同一个体, 则跳过；
+        n = n +1
+        continue
+    if group_dict[key_split[0]] ==  group_dict[key_split[1]]:  #判断是否同组,
+        print('\t'.join([key, str(distance[key]), group_dict[key_split[0]]]), file=out)
+    else:
+        print('\t'.join([key, str(distance[key]), 'Inter_group']), file=out)  #否的话，输出组间样品距离的值
+#print(n) #测试用
+out.close()
